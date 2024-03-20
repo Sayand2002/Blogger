318917481b83be9e1f374b8317dff415efbdabe8
Sayand2002 <sayandak2002@gmail.com>
2024-03-21 02:18:35 +0530

    Changed urls

--- mongoConnect.js
+++ mongoConnect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnectionString = process.env.MONGODB_URL_LOCAL
 
 const connectDB = async()=>{
-
+    
     mongoose.connect(dbConnectionString)
         .then(()=> console.log("Database Connection Successful"))
         .catch((error) => console.log("Connection Lost  : ",error));
