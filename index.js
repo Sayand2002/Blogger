4449cd90c0d3261df00dc7cdbb42657332ef47ea
Sayand2002 <sayandak2002@gmail.com>
2024-03-24 00:36:36 +0530

    UI changes

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const session = require("express-session")
 const connectDB = require("./mongoConnect");
 connectDB()
 
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
 app.use('/uploads', express.static(path.join(__dirname, '/public/uploads')));
 
 
